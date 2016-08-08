1bc58200bb30b5c7c786a036d469728abd68c54e
kaustubhhiware <hiwarekaustubh@googlemail.com>
2016-08-08 18:56:44 +0530

    Results successfully generated

--- result_gen.py
+++ result_gen.py
@@ -7,6 +7,7 @@ import io
 import random
 import csv
 import datetime
+from prettytable import PrettyTable
 
 def getLList(L,L1,L2):
 	"""
@@ -65,8 +66,8 @@ def getPredicted(Lp,L1p,L2p):
 		else:
 			L1p[node] = predom_val
 			L2p[node] = other_val
-
-	"""for node in Lp:
+	"""
+	for node in Lp:
 		if Lp[node]=="L2" or Lp[node]=="L1":
 			print "NODE :  ",node,"prediction : ",Lp[node]
 		print "with L1 : ",L1p[node],"and L2 : ",L2p[node]
@@ -131,10 +132,12 @@ def analyzeForStep(STEPnum,partnum):
 
 #report stats
 	now = datetime.datetime.now()
-	result_string = "+---\tResults for Step "+str(STEPnum)+" part "+str(partnum)+"---+"
-	result_string += "\nResults as per "+now.strftime("%d-%m-%Y %H:%M")
+	result_string = "+---\tResults for Step "+str(STEPnum)+" part "+str(partnum)+"---+\n\n"
+	result_string += "\nResults as compiled on "+now.strftime("%d-%m-%Y %H:%M")+"\n\n"
 
 	print result_string
+
+	statsTable = PrettyTable(['Detail','L1','L2'])
 #precision
 	if L1predictnum!=0:
 		precision1 = 100.0*len(L1common)/L1predictnum
@@ -147,7 +150,8 @@ def analyzeForStep(STEPnum,partnum):
 	else:
 		precision2 = "NA"
 
-	print "Precisions : ",precision1," and ",precision2
+#	print "Precisions : ",precision1," and ",precision2
+	statsTable.add_row(['Precision',precision1,precision2])
 #recall
 	if(len(L1g)!=0):
 		recall1 = 100.0*len(L1common)/len(L1g)
@@ -158,31 +162,67 @@ def analyzeForStep(STEPnum,partnum):
 	else:
 		recall2 = "NA"	
 
-	print "Recall : ",recall1," and ",recall2
+#	print "Recall : ",recall1," and ",recall2
+	statsTable.add_row(['Recall',recall1,recall2])
+
 #accuracy
 	if(len(Lg)!=0):
 		accuracy = 100.0*(len(L1common) + len(L2common))/len(Lg)
 	else:
 		accuracy = "NA"# Possible only when output not generated
-	
-	print "accuracy: ",accuracy
 
+#	print "accuracy: ",accuracy
+	statsTable.add_row(['Accuracy','=>',accuracy])
+
+	stats_text = statsTable.get_string()
+	#print stats_text
+	result_string += "\n"+stats_text+"\n"
 
 #Everything now calculated , now write this into a results folder
 
+	connectionTable = PrettyTable(['Node','Mapping','Goldlabel','Predict',
+		'L1 confidence','L2confidence'])
 	#make connections between edges and their code names
 	with open("nodes_dict.txt") as f:
 		dic = json.loads(f.read())
 
 		for each in dic:
 			for x in dic[each]:
-				print "N",each,"connects\t",x,"to",dic[each][x][0].split(" ")[0]
+				#print "N",each," :",x,"->",
+				#dic[each][x][0].split(" ")[0],"L1",L1p["N"+each],"L2",L2p["N"+each]
+				index = "N"+each
+				mapping = x+"->"+dic[each][x][0].split(" ")[0]
+				if index in Lg:
+					given = Lg[index]
+				else:
+					given = "None"
 
+				if index in Lp:
+					predicted = Lp[index]
+					L1val = L1p[index]
+					L2val = L2p[index]
+				else:
+					predicted = "None"
+					L1val = "NA"
+					L2val = "NA"
 
+				connectionTable.add_row([index,mapping,given,predicted,L1val,L2val])
 
-
+	connections = connectionTable.get_string()
+	#print connections
+	result_string += "\n"+connections+"\n"
 
 	os.chdir("../..")
+	os.system("mkdir results")
+	outstr = "results/step_"+str(STEPnum)+"_part_"+str(partnum)+"_.txt"
+	
+	if os.path.isfile(outstr):
+		os.system("rm -f "+outstr)
+		print "Updating results ..."
+
+	outFile = open(outstr,'wb')		
+	outFile.write(result_string)
+	outFile.close()
 
 
 
@@ -190,6 +230,9 @@ def analyzeForStep(STEPnum,partnum):
 #MAIN PART
 
 #validate part
+print "Checking if format available.... "
+os.system("pip install PrettyTable")
+
 partnum = raw_input("Enter part num : ")
 nope = (5,10,12,18,22,24)
 partnum = int(partnum)
@@ -201,16 +244,20 @@ if partnum in range(1,31):
 else : 
 	print "not even in range"
 
-for STEPnum in range(1,2):#do for each STEP
+STEPnum = raw_input("Enter which Step : ")
+STEPnum = int(STEPnum)
+if STEPnum not in range(1,4):
+	print "Not valid"
+	quit()
 
-	base_path = "STEP"+str(STEPnum)+"/part"+str(partnum)+"/"
-	gold = "gold_labels.txt"
-	out = "label_prop_output.txt"
-	if not os.path.isfile(base_path+gold):
-		print "Gold labels missing! ...Closing"
-		quit()
-	elif not os.path.isfile(base_path+out):
-		print "Output file missing! ...Closing"
-		quit()
-	else:
-		analyzeForStep(STEPnum,partnum)
+base_path = "STEP"+str(STEPnum)+"/part"+str(partnum)+"/"
+gold = "gold_labels.txt"
+out = "label_prop_output.txt"
+if not os.path.isfile(base_path+gold):
+	print "Gold labels missing! ...Closing"
+	quit()
+elif not os.path.isfile(base_path+out):
+	print "Output file missing! ...Closing"
+	quit()
+else:
+	analyzeForStep(STEPnum,partnum)
